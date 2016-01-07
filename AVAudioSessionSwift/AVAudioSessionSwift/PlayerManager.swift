8ff0af6db03c07c480a4ea62b7e1c0b0a23e1644
machao <490521687@qq.com>
2016-01-07 09:22:12 +0800

    add some func

--- AVAudioSessionSwift/AVAudioSessionSwift/PlayerManager.swift
+++ AVAudioSessionSwift/AVAudioSessionSwift/PlayerManager.swift
@@ -10,7 +10,9 @@ import UIKit
 import AVFoundation
 class PlayerManager: NSObject {
 
-    
+    var playing = false
+    var players: [AVAudioPlayer]!
+
     func playerForFile(name:String) -> AVAudioPlayer {
         
         let fileURL = NSBundle.mainBundle().URLForResource(name, withExtension: "caf")!
@@ -26,4 +28,30 @@ class PlayerManager: NSObject {
             fatalError()
         }
     }
+    
+    func play() {
+        if !playing {
+            let delayTime = players.first!.deviceCurrentTime + 0.01
+            for player in players {
+                player.playAtTime(delayTime)
+            }
+            playing = true
+        }
+    }
+    func stop (){
+        if playing {
+            for player in players {
+                player.stop()
+                player.currentTime = 0.0
+            }
+            playing = false
+        }
+    }
+    func adjuctRate(rate:Double) {
+        for player in players {
+            player.rate = Float(rate)
+        }
+    }
+    
+    
 }
